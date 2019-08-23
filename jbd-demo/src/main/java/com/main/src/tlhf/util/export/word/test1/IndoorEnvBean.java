c60eee40f104a2e13fcfcead7ea6910878560ecd
daviddingjb <157517301@qq.com>
2019-08-23 14:25:12 +0800

    jbding

---
+++ jbd-demo/src/main/java/com/main/src/tlhf/util/export/word/test1/IndoorEnvBean.java
@@ -0,0 +1,340 @@
+package com.main.src.tlhf.util.export.word.test1;
+
+
+public class IndoorEnvBean {
+	private String tlhf_name;        // 检测公司
+	private String entrusted_name;   // 委托单位
+	private String obj_name;         // 工程名称
+	private String const_name;       // 建设单位
+	private String design_name;      // 设计单位
+	private String const_unit_name;  // 施工单位
+	private String supervision_name; // 监理单位
+	private String struc_type;       // 结构类型
+	private String level;            // 层数
+	private String area;             // 建筑面积（m²）
+	private String check_type;       // 检测类型
+	private String bh_year;          // 编号_年
+	private String bh_nm;            // 编号
+	private String qf_year;          // 签发日期_年
+	private String qf_month;         // 签发日期_月
+	private String qf_day;           // 签发日期_日
+	private String obj_addr;         // 工程地址
+	private String check_list;       // 检测项目列表
+	private String conclusion;       // 符合
+	
+	private String check_nm;         // 检测编号
+	private String check_nm_start;            // 合并单元格
+	private String check_nm_end;              // 合并单元格
+	private String check_name;       // 检测项目
+	private String check_name_start;            // 合并单元格
+	private String check_name_end;              // 合并单元格
+	private String limit_value;      // 限量值
+	private String limit_value_start;            // 合并单元格
+	private String limit_value_end;              // 合并单元格
+	private String check_station;    // 位置
+	private String check_station_start;            // 合并单元格
+	private String check_station_end;              // 合并单元格
+	private String check_point;      // 测点号
+	private String check_value;      // 实测结果（mg/m3）_测点值
+	private String check_average;    // 实测结果（mg/m3）_平均值
+	private String check_average_start;            // 合并单元格
+	private String check_average_end;              // 合并单元格
+	private String check_rtn;        // 本项结论
+
+	public String getTlhf_name() {
+		return tlhf_name;
+	}
+
+	public void setTlhf_name(String tlhf_name) {
+		this.tlhf_name = tlhf_name;
+	}
+
+	public String getBh_year() {
+		return bh_year;
+	}
+
+	public void setBh_year(String bh_year) {
+		this.bh_year = bh_year;
+	}
+
+	public String getBh_nm() {
+		return bh_nm;
+	}
+
+	public void setBh_nm(String bh_nm) {
+		this.bh_nm = bh_nm;
+	}
+
+	public String getObj_name() {
+		return obj_name;
+	}
+
+	public void setObj_name(String obj_name) {
+		this.obj_name = obj_name;
+	}
+
+	public String getEntrusted_name() {
+		return entrusted_name;
+	}
+
+	public void setEntrusted_name(String entrusted_name) {
+		this.entrusted_name = entrusted_name;
+	}
+
+	public String getQf_year() {
+		return qf_year;
+	}
+
+	public void setQf_year(String qf_year) {
+		this.qf_year = qf_year;
+	}
+
+	public String getQf_month() {
+		return qf_month;
+	}
+
+	public void setQf_month(String qf_month) {
+		this.qf_month = qf_month;
+	}
+
+	public String getQf_day() {
+		return qf_day;
+	}
+
+	public void setQf_day(String qf_day) {
+		this.qf_day = qf_day;
+	}
+
+	public String getObj_addr() {
+		return obj_addr;
+	}
+
+	public void setObj_addr(String obj_addr) {
+		this.obj_addr = obj_addr;
+	}
+
+	public String getStruc_type() {
+		return struc_type;
+	}
+
+	public void setStruc_type(String struc_type) {
+		this.struc_type = struc_type;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getConst_name() {
+		return const_name;
+	}
+
+	public void setConst_name(String const_name) {
+		this.const_name = const_name;
+	}
+
+	public String getDesign_name() {
+		return design_name;
+	}
+
+	public void setDesign_name(String design_name) {
+		this.design_name = design_name;
+	}
+
+	public String getConst_unit_name() {
+		return const_unit_name;
+	}
+
+	public void setConst_unit_name(String const_unit_name) {
+		this.const_unit_name = const_unit_name;
+	}
+
+	public String getSupervision_name() {
+		return supervision_name;
+	}
+
+	public void setSupervision_name(String supervision_name) {
+		this.supervision_name = supervision_name;
+	}
+
+	public String getCheck_list() {
+		return check_list;
+	}
+
+	public void setCheck_list(String check_list) {
+		this.check_list = check_list;
+	}
+
+	public String getCheck_type() {
+		return check_type;
+	}
+
+	public void setCheck_type(String check_type) {
+		this.check_type = check_type;
+	}
+
+	public String getConclusion() {
+		return conclusion;
+	}
+
+	public void setConclusion(String conclusion) {
+		this.conclusion = conclusion;
+	}
+
+	public String getCheck_nm() {
+		return check_nm;
+	}
+
+	public void setCheck_nm(String check_nm) {
+		this.check_nm = check_nm;
+	}
+
+	public String getCheck_name() {
+		return check_name;
+	}
+
+	public void setCheck_name(String check_name) {
+		this.check_name = check_name;
+	}
+
+	public String getLimit_value() {
+		return limit_value;
+	}
+
+	public void setLimit_value(String limit_value) {
+		this.limit_value = limit_value;
+	}
+
+	public String getCheck_station() {
+		return check_station;
+	}
+
+	public void setCheck_station(String check_station) {
+		this.check_station = check_station;
+	}
+
+	public String getCheck_point() {
+		return check_point;
+	}
+
+	public void setCheck_point(String check_point) {
+		this.check_point = check_point;
+	}
+
+	public String getCheck_value() {
+		return check_value;
+	}
+
+	public void setCheck_value(String check_value) {
+		this.check_value = check_value;
+	}
+
+	public String getCheck_average() {
+		return check_average;
+	}
+
+	public void setCheck_average(String check_average) {
+		this.check_average = check_average;
+	}
+
+	public String getCheck_rtn() {
+		return check_rtn;
+	}
+
+	public void setCheck_rtn(String check_rtn) {
+		this.check_rtn = check_rtn;
+	}
+
+	public String getCheck_nm_start() {
+		return check_nm_start;
+	}
+
+	public void setCheck_nm_start(String check_nm_start) {
+		this.check_nm_start = check_nm_start;
+	}
+
+	public String getCheck_nm_end() {
+		return check_nm_end;
+	}
+
+	public void setCheck_nm_end(String check_nm_end) {
+		this.check_nm_end = check_nm_end;
+	}
+
+	public String getCheck_name_start() {
+		return check_name_start;
+	}
+
+	public void setCheck_name_start(String check_name_start) {
+		this.check_name_start = check_name_start;
+	}
+
+	public String getCheck_name_end() {
+		return check_name_end;
+	}
+
+	public void setCheck_name_end(String check_name_end) {
+		this.check_name_end = check_name_end;
+	}
+
+	public String getLimit_value_start() {
+		return limit_value_start;
+	}
+
+	public void setLimit_value_start(String limit_value_start) {
+		this.limit_value_start = limit_value_start;
+	}
+
+	public String getLimit_value_end() {
+		return limit_value_end;
+	}
+
+	public void setLimit_value_end(String limit_value_end) {
+		this.limit_value_end = limit_value_end;
+	}
+
+	public String getCheck_station_start() {
+		return check_station_start;
+	}
+
+	public void setCheck_station_start(String check_station_start) {
+		this.check_station_start = check_station_start;
+	}
+
+	public String getCheck_station_end() {
+		return check_station_end;
+	}
+
+	public void setCheck_station_end(String check_station_end) {
+		this.check_station_end = check_station_end;
+	}
+
+	public String getCheck_average_start() {
+		return check_average_start;
+	}
+
+	public void setCheck_average_start(String check_average_start) {
+		this.check_average_start = check_average_start;
+	}
+
+	public String getCheck_average_end() {
+		return check_average_end;
+	}
+
+	public void setCheck_average_end(String check_average_end) {
+		this.check_average_end = check_average_end;
+	}
+
+}
